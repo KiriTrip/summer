8f36c74d11ab3a8dd90f27a6af2dfd5b2b11bc8b
KiriTrip <eggor01042010@gmail.com>
2025-07-07 16:43:42 +0300

    Create Generator.hpp

---
+++ Generator.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+class Generator
+{
+public: 
+	static void genLinear(int size);
+	static void genUnLinear(int size);
+	static void genEven(int size);
+	static void genOdd(int size);
+	static void genRand(int size);
+};
