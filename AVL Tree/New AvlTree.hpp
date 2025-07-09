559b8bdf6fd0984f423fa02f804b75eac0b2c47e
KiriTrip <eggor01042010@gmail.com>
2025-07-09 17:48:00 +0300

    Create New AvlTree.hpp

---
+++ AVL Tree/New AvlTree.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+class Node {
+public:
+	int value;
+	int height;
+	Node* l, * r;
+
+	Node();
+	Node(int value);
+	Node(int value, Node* l, Node* r);
+	~Node();
+
+};
+
+class AvlTree {
+public:
+
+	AvlTree();
+	AvlTree(int value);
+	AvlTree(Node* root);
+	~AvlTree();
+
+	void insRec(int value);
+
+	int rootHeight();
+
+	int select(int index);
+
+	bool remove(int value);
+private:
+
+	Node* root;
+	int size;
+
+	int getHeight(Node* node);
+	void updateHeight(Node* node);
+	int bal_factor(Node* node);
+	int search(int index, Node* node);
+
+	Node* fMin(Node* node);
+	Node* rmMin(Node* node);
+
+	Node* rebalance(Node* node);
+	Node* leftRotate(Node* node);
+	Node* rigthRotate(Node* node);
+
+	Node* recInsert(Node* node, int val);
+
+
+};
